e32183ceb0a3395264117bf3133262a51a8e8f90
ADISAKBOONMARK <adisak0661013@gmail.com>
2025-02-14 01:15:39 +0700

    feat: 🎸 add clear step

--- task/compile.ts
+++ task/compile.ts
@@ -1,4 +1,4 @@
-import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
+import { TASK_CLEAN, TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
 import { task } from "hardhat/config";
 
 import { setDefaultConfig } from "../src/utils";
@@ -6,9 +6,13 @@ import { setDefaultConfig } from "../src/utils";
 task(TASK_COMPILE)
   .addFlag("noTaskGen", "Don't task-gen after running this task")
   .setAction(async function (args, hre, runSuper) {
-    await runSuper();
-
     const taskGenConf = setDefaultConfig(hre.userConfig.taskGen);
+
+    if (taskGenConf.clear) {
+      await hre.run(TASK_CLEAN);
+    }
+
+    await runSuper();
 
     if (taskGenConf.runOnCompile && !args.noTaskGen) {
       await hre.run("task-gen", { noCompile: true });
